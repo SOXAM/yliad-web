91e7447449d499428bc3dc077d4178512517c7fb
JunYoung Chang <bassist@kakao.com>
2024-08-28 23:58:55 +0900

    [feat] Add tailwind header and sign-in

---
+++ src/app/video/page.tsx
@@ -0,0 +1,18 @@
+
+export default function Video() {
+    return (
+      <>
+        <div className="min-h-full">
+          <header className="bg-white shadow">
+            <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+              <h1 className="text-3xl font-bold tracking-tight text-gray-900">Video</h1>
+            </div>
+          </header>
+          <main>
+            <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">{/* Your content */}</div>
+          </main>
+        </div>
+      </>
+    )
+  }
+  
